c70aa3dcd344604b6ee264514b9f5cb973e49fe7
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-11 17:26:32 -0600

    Passes test for fourth visitor story

--- spec/feature/index_spec.rb
+++ spec/feature/index_spec.rb
@@ -3,28 +3,27 @@ require 'rails_helper'
 describe "As a visitor" do 
   describe "when I visit /shelters" do
     include Capybara::DSL
-    it "I see the name of each shelter in the system" do
+    it "I see a link to create a new shelter" do
 
       visit '/shelters'
 
-    end  
-  end
-  describe "when I visit /shelters/:id" do
-    include Capybara::DSL
-    it "I see the shelter with that id including the shelter's:
-          - name
-          - address
-          - city
-          - state
-          - zip" do
+      shelter = Shelter.create(name: "Sloans", address: "123 Main St", city: "Denver", state: "CO", zip: "git statu")
 
-      visit '/shelters/:id'
-
-      expect(page).to have_content("name")
-      expect(page).to have_content("address")
-      expect(page).to have_content("city")
-      expect(page).to have_content("state")
-      expect(page).to have_content("zip")
+      expect(page).to have_link("New Shelter")
+      click_link("New Shelter")
+      expect(current_path).to eq('/shelters/new')
+      fill_in 'name', with: "#{shelter.name}"
+      fill_in 'address', with: "#{shelter.address}"
+      fill_in 'city', with: "#{shelter.city}"
+      fill_in 'state', with: "#{shelter.state}"
+      fill_in 'zip', with: "#{shelter.zip}"
+      click_on("Create Shelter")
+      expect(current_path).to eq('/shelters')
+      expect(page).to have_content(shelter.name)
+      expect(page).to have_content(shelter.address)
+      expect(page).to have_content(shelter.city)
+      expect(page).to have_content(shelter.state)
+      expect(page).to have_content(shelter.zip)
     end  
   end
 end
